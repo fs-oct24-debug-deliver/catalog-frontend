1a92d5c6f3c2d5a4c33faf125de8124a48a330e6
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2025-01-09 09:37:31 +0100

    implement Not Found Page

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFoundPage.scss';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <div className="not-found">
+      <p className="not-found__text">
+        We couldn’t find the page you were looking for
+      </p>
+    </div>
+  );
+};
