1dc92185709e510d56303763a51fa19ff3b8f48b
OkMoroz <k.moroz.mail@gmail.com>
2025-01-11 00:09:58 +0200

    added store

---
+++ src/app/store.ts
@@ -0,0 +1,27 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import favoriteReducer from '../features/favoritesSlice';
+import cartReducer from '../features/cartSlice';
+
+const rootReducer = combineReducers({
+  favorites: favoriteReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart', 'favorites'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persist = persistStore(store);
+
+export { store, persist };
