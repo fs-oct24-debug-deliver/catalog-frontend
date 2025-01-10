54d4911693bf16e9b4fdf25751516957363f15ab
SemenVodolazskij <vodolazskijsemen@gmail.com>
2025-01-10 16:50:39 +0200

    delete-padding-inline

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -2,16 +2,14 @@ import styles from './CartPage.module.scss';
 
 export const CartPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <div className={styles.container_back}>
         <button className={styles.button}>
           <img src="../public/img/icons/arrow-back.svg"></img>
         </button>
         <span>Back</span>
       </div>
-
       <h1 className={`h1`}>Cart</h1>
-
       <div className={styles.wrapper}>
         <div className={styles.card_wrapper}>
           <div className={styles.card}>PhoneCard</div>
@@ -20,6 +18,6 @@ export const CartPage = () => {
         </div>
         <div className={styles.checkout}>CHECKOUT</div>
       </div>
-    </div>
+    </>
   );
 };
