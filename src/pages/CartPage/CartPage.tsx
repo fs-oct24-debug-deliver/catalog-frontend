78c336109a108d5877ae9c801bfa79cc12163a41
Daniel Kononchuk <11fm2020arli@gmail.com>
2025-01-10 22:26:37 +0200

    fix merge conflict

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,10 +1,7 @@
-import { CartItem } from '../../components/CartItem/CartItem';
-
 export const CartPage = () => {
   return (
     <div>
       <h1>CartPage</h1>
-      <CartItem />
     </div>
   );
 };
