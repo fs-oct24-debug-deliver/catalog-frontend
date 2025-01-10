62b4fd50955cfc752c20dfa5cd2c38e316411201
OkMoroz <k.moroz.mail@gmail.com>
2025-01-10 11:58:15 +0200

    added favicon fixed workflows

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
 import './App.module.scss';
 
-const App = () => <h1>Debug & Deliver</h1>;
+const App = () => {
+  return (
+    <div>
+      {/* <Header /> */}
+      <Outlet />
+      {/* <Footer /> */}
+    </div>
+  );
+};
 
 export default App;
