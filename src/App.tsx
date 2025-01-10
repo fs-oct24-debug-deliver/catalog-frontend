71e6f4d86939cb8bc81399f669d17efecf465e35
Daniel Kononchuk <11fm2020arli@gmail.com>
2025-01-10 23:09:12 +0200

    add cart item to cart page and fix s tyles on cart scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from './components/Header.tsx/Header';
+import { Header } from './components/Header/Header';
 import { Footer } from './components/FooterComponent/Footer';
 
 const App = () => (
