97bbac8423c6b70349bd3d1e5911712193eae74b
SemenVodolazskij <vodolazskijsemen@gmail.com>
2025-01-10 15:51:56 +0200

    implement-cart-page-and-adaptive-layout

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,7 +1,25 @@
+import styles from './CartPage.module.scss';
+
 export const CartPage = () => {
   return (
-    <div>
-      <h1>CartPage</h1>
+    <div className={styles.container}>
+      <div className={styles.container_back}>
+        <button className={styles.button}>
+          <img src="../public/img/icons/arrow-back.svg"></img>
+        </button>
+        <span>Back</span>
+      </div>
+
+      <h1 className={styles.title}>Cart</h1>
+
+      <div className={styles.wrapper}>
+        <div className={styles.card_wrapper}>
+          <div className={styles.card}>PhoneCard</div>
+          <div className={styles.card}>PhoneCard</div>
+          <div className={styles.card}>PhoneCard</div>
+        </div>
+        <div className={styles.checkout}>CHECKOUT</div>
+      </div>
     </div>
   );
 };
