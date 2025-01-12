a8971e089797837bb2617fc28624d2a0a51b3174
OkMoroz <k.moroz.mail@gmail.com>
2025-01-12 02:01:33 +0200

    added About section and fixed Button Back

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,7 +1,19 @@
+// import detailStyle from './ProductDetailsPage.module.scss';
+import { ButtonBack } from '../../components/ButtonBack';
+// import { Gallery } from '../../components/Gallery';
+// import { Characteristics } from '../../components/Characteristics';
+import { AboutSection } from './components/AboutSection/AboutSection';
+// import { TechSpecs } from './components/AboutSection/AboutSection';
+
 export const ProductDetailsPage = () => {
   return (
     <div>
       <h1>ProductDetailsPage</h1>
+      <ButtonBack />
+      {/* <Gallery />
+      <Characteristics /> */}
+      <AboutSection />
+      {/* <TechSpecs /> */}
     </div>
   );
 };
