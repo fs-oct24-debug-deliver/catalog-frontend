126d7bce7b4e162f7efb372de219af1b0eb92c33
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2025-01-12 12:17:52 +0100

    fix swiper padding and import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/FooterComponent/Footer';
 import styles from './App.module.scss';
-import { HomePageSwiper } from './pages/HomePage/components/HomePageSwiper';
 
 const App = () => (
   <>
@@ -10,7 +9,6 @@ const App = () => (
     <div className={styles.container}>
       <Outlet />
     </div>
-    <HomePageSwiper />
     <Footer />
   </>
 );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,12 @@
+import { HomePageSwiper } from './components/HomePageSwiper';
+
 export const HomePage = () => {
   return (
     <div>
       <h1>Welcome to Nice Gadgets store!</h1>
-      <section></section>
+      <section>
+        <HomePageSwiper />
+      </section>
       {/* <ProductsSlider />  Brand new models*/}
       {/* <ShopByCategory />  Shop by category*/}
       {/* <ProductsSlider />  Hot prices*/}
