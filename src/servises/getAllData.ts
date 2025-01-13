c37f0d72ee7d59539c6e823cc11d1a331035463a
Oleksandr Kotlyar <kotlyar7733@gmail.com>
2025-01-13 17:24:36 +0200

    loading information from API

--- src/servises/getAllData.ts
+++ src/servises/getAllData.ts
@@ -1,4 +1,9 @@
 import { Card } from '../types/Card';
+import { Product } from '../types/Product';
 import { getData } from '../utils/httpClient';
 
 export const getAllProducts = getData<Card[]>('/products.json');
+
+export const getAllPhones = getData<Product[]>('/phones.json');
+export const getAllTablets = getData<Product[]>('/tablets.json');
+export const getAllAccessories = getData<Product[]>('/accessories.json');
