f0233067b9575fbcca00d6d4f131d3fbe178101c
SemenVodolazskij <vodolazskijsemen@gmail.com>
2025-01-13 12:06:24 +0200

    implement-tech-specs-table

--- src/pages/ProductDetailsPage/components/TechSpecs/TechSpecs.tsx
+++ src/pages/ProductDetailsPage/components/TechSpecs/TechSpecs.tsx
@@ -1,31 +1,40 @@
 import styles from './TechSpecs.module.scss';
 
+const data = [
+  { title: 'Screen', description: '6.5” OLED' },
+  { title: 'Resolution', description: '2688x1242' },
+  { title: 'Processor', description: 'Apple A12 Bionic' },
+  { title: 'RAM', description: '3 GB' },
+  { title: 'Built in memory', description: '64 GB' },
+  { title: 'Camera', description: '12 Mp + 12 Mp + 12 Mp (Triple)' },
+  { title: 'Zoom', description: 'Optical, 2x' },
+  { title: 'Ceil', description: 'GSM, LTE, UMTS' },
+];
+
 export const TechSpecs = () => {
   return (
     <>
       <h4 className={`${styles.title} h4`}>Tech specs</h4>
       <div className={styles.wrapper}>
         <dl className={styles.tech_specs}>
-          <dt className={styles.tech_specs_title}>Screen</dt>
-          <dt className={styles.tech_specs_title}>Resolution</dt>
-          <dt className={styles.tech_specs_title}>Processor</dt>
-          <dt className={styles.tech_specs_title}>RAM</dt>
-          <dt className={styles.tech_specs_title}>Built in memory</dt>
-          <dt className={styles.tech_specs_title}>Camera</dt>
-          <dt className={styles.tech_specs_title}>Zoom</dt>
-          <dt className={styles.tech_specs_title}>Ceil</dt>
+          {data.map((spec, index) => (
+            <dt
+              key={index}
+              className={styles.tech_specs_title}
+            >
+              {spec.title}
+            </dt>
+          ))}
         </dl>
         <dl className={styles.tech_specs}>
-          <dd className={styles.tech_specs_description}>6.5” OLED</dd>
-          <dd className={styles.tech_specs_description}>2688x1242</dd>
-          <dd className={styles.tech_specs_description}>Apple A12 Bionic</dd>
-          <dd className={styles.tech_specs_description}>3 GB</dd>
-          <dd className={styles.tech_specs_description}>64 GB</dd>
-          <dd className={styles.tech_specs_description}>
-            12 Mp + 12 Mp + 12 Mp (Triple)
-          </dd>
-          <dd className={styles.tech_specs_description}>Optical, 2x</dd>
-          <dd className={styles.tech_specs_description}>GSM, LTE, UMTS</dd>
+          {data.map((spec, index) => (
+            <dd
+              key={index}
+              className={styles.tech_specs_description}
+            >
+              {spec.description}
+            </dd>
+          ))}
         </dl>
       </div>
     </>
