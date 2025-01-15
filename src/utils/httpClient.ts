412b904579d260ab84e90528ceaf857b1dcdff4f
Oleksandr Kotlyar <kotlyar7733@gmail.com>
2025-01-15 16:25:53 +0200

    fixing api

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://fs-oct24-debug-deliver.netlify.app/api';
+const BASE_URL = '/api';
 
 export function getData<T>(url: string): Promise<T> {
-  return fetch(BASE_URL + url, { mode: 'no-cors' }).then((response) => {
+  return fetch(BASE_URL + url).then((response) => {
     console.log(response);
     if (!response.ok) {
       throw new Error(`${response.status} ${response.statusText}`);
