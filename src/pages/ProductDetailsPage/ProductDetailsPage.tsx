41b97cb9d7b21ece65907d50488721366642fbdc
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2025-01-15 12:14:28 +0100

    fixes

--- src/pages/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,30 +1,24 @@
-// import detailStyle from './ProductDetailsPage.module.scss';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { getProductById } from '../../servises/productFunctions';
-import { ButtonBack } from '../../components/ButtonBack';
-// import { Gallery } from '../../components/Gallery';
-// import { Characteristics } from '../../components/Characteristics';
-import { AboutSection } from './components/AboutSection/AboutSection';
 import {
   getProductById,
   getProductsByCategory,
 } from '../../servises/productFunctions';
-import { Product } from '../../types/Product';
-import { Loader } from '../../components/Loader';
-import { Card } from '../../types/Card';
-import { SwiperComponent } from '../../components/Swiper/Swiper';
-// import { TechSpecs } from './components/AboutSection/AboutSection';
-
-// type Props = {
-//   id: string;
-//   category: string;
-// };
+import { ButtonBack } from '../../components/ButtonBack';
+import { AboutSection } from './components/AboutSection';
+import productDetailsStyles from './ProductDetailsPage.module.scss';
+import { Gallery } from './components/Gallery';
+import { Product } from '../../types/Product.ts';
+import { Loader } from '../../components/Loader/Loader.tsx';
+import { SwiperComponent } from '../../components/Swiper/Swiper.tsx';
+import { Card } from '../../types/Card.ts';
+import { Breadcrumbs } from '../../components/Breadcrumbs/Breadcrumbs.tsx';
 
 export const ProductDetailsPage = () => {
   // const { id, category } = props;
   const [product, setProduct] = useState<Product | null>(null);
   const [errorMessage, setErrorMessage] = useState('');
+
   const [isLoading, setIsLoading] = useState(false);
   const [recomendedProducts, setRecomendedProducts] = useState<Card[]>();
   const [isLoadingRecomendedProducts, setIsLoadingRecomendedProducts] =
@@ -47,27 +41,46 @@ export const ProductDetailsPage = () => {
       )
       .finally(() => setIsLoading(false));
   }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getProductsByCategory('phones')
+      .then((products) => setRecomendedProducts(products.slice(0, 8)))
+      .catch(() => console.warn('Error to load '))
+      .finally(() => setIsLoadingRecomendedProducts(false));
+
+    getProductById('apple-iphone-11-128gb-green', 'phones')
+      .then(setProduct)
+      .catch(() =>
+        setErrorMessage('Failed to load this product. Please try again later.'),
+      )
+      .finally(() => setIsLoading(false));
+  }, []);
   return (
-    <>
-      {errorMessage ?
-        <h1>{errorMessage}</h1>
-      : <div>
-          <h1>ProductDetailsPage</h1>
-          <ButtonBack />
-          {isLoading && <Loader />}
-          <p>{product?.name}</p>
-          {/* <Gallery />
-          <Characteristics /> */}
-          <AboutSection />
-          {/* <TechSpecs /> */}
-          {!isLoadingRecomendedProducts && recomendedProducts?.length && (
-            <SwiperComponent
-              cards={recomendedProducts}
-              title="You may also like"
-            />
-          )}
-        </div>
-      }
-    </>
+    <div>
+      <Breadcrumbs />
+      <ButtonBack />
+      <h1 className={productDetailsStyles.title}>{product.name}</h1>
+      <Gallery images={product.images} />
+      <AboutSection
+        description={product.description}
+        specs={{
+          screen: product.screen,
+          resolution: product.resolution,
+          processor: product.processor,
+          ram: product.ram,
+          camera: product.camera,
+          zoom: product.zoom,
+          cell: product.cell,
+        }}
+      />
+      {!isLoadingRecomendedProducts && recomendedProducts?.length && (
+        <SwiperComponent
+          cards={recomendedProducts}
+          title="You may also like"
+        />
+      )}
+    </div>
   );
 };
