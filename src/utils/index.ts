7b64fad550e61de38bd6cc4e6091786e56f83545
Daniel Kononchuk <11fm2020arli@gmail.com>
2025-01-16 20:26:45 +0200

    fix select styles and add styles for light theme

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,12 @@ export const getNavLinkClass = (
   activeClass: string,
   defaultClass: string,
 ) => (isActive ? activeClass : defaultClass);
+
+export function hexToRgba(hex: string, alpha: number): string {
+  const [r, g, b] = hex
+    .replace(/^#/, '')
+    .match(/.{1,2}/g)!
+    .map((x) => parseInt(x, 16));
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
