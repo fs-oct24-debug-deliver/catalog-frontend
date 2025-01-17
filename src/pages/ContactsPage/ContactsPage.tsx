7af4d57a5d9948f3497badff6382325705dcef75
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2025-01-17 12:25:07 +0100

    feat contacts page

---
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -0,0 +1,22 @@
+export const ContactsPage = () => {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur non
+      reprehenderit fugit voluptate deserunt, eum temporibus itaque sequi
+      dolores impedit quod praesentium quae voluptatem accusamus. Nobis fuga
+      natus repellendus voluptatibus. Deserunt quo tenetur perspiciatis
+      voluptatem veritatis exercitationem ad ipsum aut assumenda cum fugit eos
+      nam, laboriosam at magnam eum totam esse maxime voluptate debitis
+      necessitatibus sint? Ipsum dicta harum tenetur! Illum mollitia non porro
+      debitis, beatae a aut veniam deleniti. Explicabo eveniet soluta illum
+      quaerat pariatur atque ea tenetur, quidem, ab voluptate quos, tempore quo?
+      Nobis molestiae a error adipisci? Dolore ratione laboriosam totam dolores
+      fuga doloribus eligendi consequatur exercitationem, voluptas maxime? Neque
+      cum esse autem voluptas id, repellat sit provident nam facere iure ea
+      delectus rem porro quae omnis! Totam debitis ullam dignissimos aut
+      deleniti inventore hic ea qui amet distinctio! Tempore harum veniam, aut,
+      illo eum enim fugiat incidunt doloribus veritatis modi accusantium
+      praesentium repellat nam tempora corrupti.
+    </p>
+  );
+};
